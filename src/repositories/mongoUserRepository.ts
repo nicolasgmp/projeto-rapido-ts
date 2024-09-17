d4daa7515c7086ffee82b1a17f146482988a388d
magrininicolas <magrininicolas@outlook.com>
2024-09-17 15:08:04 -0300

    feat: ensure UserRepository get users from mongo

--- src/repositories/mongoUserRepository.ts
+++ src/repositories/mongoUserRepository.ts
@@ -4,14 +4,8 @@ import { IUserRepository } from "./interfaces/iUserRepository";
 
 export class MongoUserRepository implements IUserRepository {
   async findAll(): Promise<User[]> {
-    const users = await MongoClientDB.db.collection<User>("users").find({}).toArray();
-    return [
-      {
-        firstName: "Nicolas",
-        lastName: "Pereira",
-        email: "nicolas@email.com",
-        password: "12345",
-      },
-    ];
+    const users = await MongoClientDB.db.collection<Omit<User, "id">>("users").find({}).toArray();
+
+    return users.map(({ _id, ...rest }) => ({ ...rest, id: _id.toHexString() }));
   }
 }
